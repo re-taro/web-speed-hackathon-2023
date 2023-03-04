37b260ad8996f4648be7244b226a9b273d46c2c1
Rintaro Itokawa <me@re-taro.dev>
2023-03-05 03:56:13 +0900

    refactor: subset font

--- src/client/pages/NotFound/NotFound.tsx
+++ src/client/pages/NotFound/NotFound.tsx
@@ -1,36 +1,17 @@
-import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
-import { loadFonts } from '../../utils/load_fonts';
-
 import * as styles from './NotFound.styles';
 import type { FC } from 'react';
 
 export const NotFound: FC = () => {
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    const load = async() => {
-      await loadFonts();
-      setIsReady(true);
-    };
-
-    load();
-  }, []);
-
-  if (!isReady)
-    return null;
-
   return (
     <>
       <Helmet>
         <title>ページが見つかりませんでした</title>
       </Helmet>
-      <div>
-        <div className={styles.container()}>
-          <div className={styles.inner()}>
-            <p className={styles.mainParagraph()}>ページが存在しません</p>
-            <p className={styles.subParagraph()}>Not Found</p>
-          </div>
+      <div className={styles.container()}>
+        <div className={styles.inner()}>
+          <p className={styles.mainParagraph()}>ページが存在しません</p>
+          <p className={styles.subParagraph()}>Not Found</p>
         </div>
       </div>
     </>
--- src/client/pages/OrderComplete/OrderComplete.tsx
+++ src/client/pages/OrderComplete/OrderComplete.tsx
@@ -1,5 +1,4 @@
 import classNames from 'classnames';
-import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { useNavigate } from 'react-router-dom';
 import { AspectRatio } from '../../components/foundation/AspectRatio/AspectRatio';
@@ -9,21 +8,14 @@ import { WidthRestriction } from '../../components/foundation/WidthRestriction/W
 import { ProductHeroImage } from '../../components/product/ProductHeroImage/ProductHeroImage';
 import { useAuthUser } from '../../hooks/useAuthUser';
 import { useRecommendation } from '../../hooks/useRecommendation';
-import { loadFonts } from '../../utils/load_fonts';
 import * as styles from './OrderComplete.styles';
 import type { FC } from 'react';
 
 export const OrderComplete: FC = () => {
   const navigate = useNavigate();
-  const [isReadyFont, setIsReadyFont] = useState(false);
   const { authUserLoading, isAuthUser } = useAuthUser();
   const { recommendation } = useRecommendation();
-  useEffect(() => {
-    loadFonts().then(() => {
-      setIsReadyFont(true);
-    });
-  }, []);
-  if (!recommendation || !isReadyFont || authUserLoading)
+  if (!recommendation || authUserLoading)
     return null;
   if (!isAuthUser) {
     navigate('/');
@@ -35,40 +27,38 @@ export const OrderComplete: FC = () => {
       <Helmet>
         <title>購入が完了しました</title>
       </Helmet>
-      <div>
-        <GetDeviceType>
-          {({ deviceType }) => (
-            <WidthRestriction>
-              <div className={styles.container()}>
-                <div className={styles.notice()}>
-                  <h2 className={styles.noticeHeading()}>購入が完了しました</h2>
-                  <AspectRatio ratioHeight={1} ratioWidth={2}>
-                    <div className={styles.noticeDescriptionWrapper()}>
-                      <p
-                        className={classNames(styles.noticeDescription(), {
-                          [styles.noticeDescription__desktop()]: deviceType === DeviceT.DESKTOP,
-                          [styles.noticeDescription__mobile()]: deviceType === DeviceT.MOBILE,
-                        })}
-                      >
-                        このサイトは架空のサイトであり、商品が発送されることはありません
-                      </p>
-                    </div>
-                  </AspectRatio>
-                </div>
-                <div className={styles.recommended()}>
-                  <h2 className={styles.recommendedHeading()}>こちらの商品もオススメです</h2>
-                  <ProductHeroImage product={recommendation.product} title={recommendation.product.name} />
-                </div>
-                <div className={styles.backToTopButtonWrapper()}>
-                  <PrimaryAnchor href="/" size="lg">
-                    トップへ戻る
-                  </PrimaryAnchor>
-                </div>
+      <GetDeviceType>
+        {({ deviceType }) => (
+          <WidthRestriction>
+            <div className={styles.container()}>
+              <div className={styles.notice()}>
+                <h2 className={styles.noticeHeading()}>購入が完了しました</h2>
+                <AspectRatio ratioHeight={1} ratioWidth={2}>
+                  <div className={styles.noticeDescriptionWrapper()}>
+                    <p
+                      className={classNames(styles.noticeDescription(), {
+                        [styles.noticeDescription__desktop()]: deviceType === DeviceT.DESKTOP,
+                        [styles.noticeDescription__mobile()]: deviceType === DeviceT.MOBILE,
+                      })}
+                    >
+                      このサイトは架空のサイトであり、商品が発送されることはありません
+                    </p>
+                  </div>
+                </AspectRatio>
               </div>
-            </WidthRestriction>
-          )}
-        </GetDeviceType>
-      </div>
+              <div className={styles.recommended()}>
+                <h2 className={styles.recommendedHeading()}>こちらの商品もオススメです</h2>
+                <ProductHeroImage product={recommendation.product} title={recommendation.product.name} />
+              </div>
+              <div className={styles.backToTopButtonWrapper()}>
+                <PrimaryAnchor href="/" size="lg">
+                  トップへ戻る
+                </PrimaryAnchor>
+              </div>
+            </div>
+          </WidthRestriction>
+        )}
+      </GetDeviceType>
     </>
   );
 };
