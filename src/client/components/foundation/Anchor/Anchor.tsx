6f7bb97cf7f26d832e00b3daa8508a773500d064
Rintaro Itokawa <me@re-taro.dev>
2023-03-05 01:25:16 +0900

    refactor: spa

--- src/client/components/foundation/Anchor/Anchor.tsx
+++ src/client/components/foundation/Anchor/Anchor.tsx
@@ -1,10 +1,24 @@
+import { Link } from 'react-router-dom';
+
 import * as styles from './Anchor.styles';
-import type { ComponentProps, FC } from 'react';
+import type { ComponentPropsWithoutRef, FC } from 'react';
 
-type Props = Omit<ComponentProps<'a'>, 'className'>;
+type Props = Omit<ComponentPropsWithoutRef<'a'>, 'className'>;
 
-export const Anchor: FC<Props> = ({ children, href, ...rest }) => (
-  <a className={styles.container()} href={href} {...rest}>
-    {children}
-  </a>
-);
+export const Anchor: FC<Props> = ({ children, href, ...rest }) => {
+  const isExternal = /^https?:\/\//.test(href ?? '');
+
+  if (isExternal) {
+    return (
+      <a className={styles.container()} href={href} {...rest}>
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <Link className={styles.container()} to={href ?? ''} {...rest}>
+      {children}
+    </Link>
+  );
+};
