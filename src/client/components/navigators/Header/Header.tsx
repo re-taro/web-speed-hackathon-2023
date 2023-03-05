e626fb5a3bd3eba4237fc4e09024dad571cc1802
Rintaro Itokawa <me@re-taro.dev>
2023-03-05 14:43:37 +0900

    refactor: wait loading for auth

--- src/client/components/navigators/Header/Header.tsx
+++ src/client/components/navigators/Header/Header.tsx
@@ -4,12 +4,11 @@ import { useOpenModal } from '../../../store/modal/hooks';
 import { Anchor } from '../../foundation/Anchor/Anchor';
 import { Icon } from '../../foundation/Icon/Icon';
 import { Image } from '../../foundation/Image/Image';
-
 import * as styles from './Header.styles';
 import type { FC } from 'react';
 
 export const Header: FC = () => {
-  const { isAuthUser } = useAuthUser();
+  const { isAuthUser, authUserLoading } = useAuthUser();
   const handleOpenModal = useOpenModal();
 
   return (
@@ -19,23 +18,24 @@ export const Header: FC = () => {
           <Image height={32} src="/icons/logo.png" srcSet="/icons/logo.png, /icons/logo@2x.png 2x" width={205} />
         </div>
       </Anchor>
-      {isAuthUser
-        ? (
-          <Anchor data-testid="navigate-order" href="/order">
-            <div className={styles.orderLink()}>
-              <Icon color="#222222" height={20} icon={FaShoppingCart} width={20} />
-            </div>
-          </Anchor>
-          )
-        : (
-          <button
-            className={styles.signInButton()}
-            data-testid="navigate-signin"
-            onClick={() => handleOpenModal('SIGN_IN')}
-          >
-            <Icon color="#222222" height={20} icon={FaUser} width={20} />
-          </button>
-          )}
+      {!authUserLoading
+        && (isAuthUser
+          ? (
+            <Anchor data-testid="navigate-order" href="/order">
+              <div className={styles.orderLink()}>
+                <Icon color="#222222" height={20} icon={FaShoppingCart} width={20} />
+              </div>
+            </Anchor>
+            )
+          : (
+            <button
+              className={styles.signInButton()}
+              data-testid="navigate-signin"
+              onClick={() => handleOpenModal('SIGN_IN')}
+            >
+              <Icon color="#222222" height={20} icon={FaUser} width={20} />
+            </button>
+            ))}
     </header>
   );
 };
