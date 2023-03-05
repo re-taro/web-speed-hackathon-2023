058ed6fed869032786f096bd38469c9a9c07832d
Rintaro Itokawa <me@re-taro.dev>
2023-03-05 14:27:10 +0900

    fix: delete sync xhr

--- src/client/utils/apollo_client.ts
+++ src/client/utils/apollo_client.ts
@@ -1,34 +1,6 @@
-/* eslint-disable prefer-promise-reject-errors */
-
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
-import type { HttpOptions } from '@apollo/client';
 
-const syncXhr: HttpOptions['fetch'] = (uri, options) => {
-  return new Promise((resolve, reject) => {
-    const method = options?.method;
-    if (method === undefined)
-      return reject();
-
-    const body = options?.body;
-    if (body instanceof ReadableStream)
-      return reject();
-
-    const request = new XMLHttpRequest();
-    request.open(method, uri.toString(), false);
-    request.setRequestHeader('content-type', 'application/json');
-    request.onload = () => {
-      if (request.status >= 200 && request.status < 300)
-        return resolve(new Response(request.response));
-
-      reject();
-    };
-    request.onerror = reject;
-
-    request.send(body);
-  });
-};
-
-const link = new HttpLink({ fetch: syncXhr });
+const link = new HttpLink();
 
 export const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
@@ -48,5 +20,3 @@ export const apolloClient = new ApolloClient({
   queryDeduplication: false,
   uri: '/graphql',
 });
-
-/* eslint-enable prefer-promise-reject-errors */
